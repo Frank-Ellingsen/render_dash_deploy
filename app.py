3e6e7b83ef9119e411aaafe97b8d3b0f17daf564
FrankE <frankellingsen@hotmail.com>
2025-08-05 21:58:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -95,4 +95,5 @@ def update_graph(option_slctd):
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
-    app.run(debug=True))  
+
+    app.run(debug=True)
